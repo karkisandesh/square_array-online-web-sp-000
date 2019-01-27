393bcfd45c382406f1d3b5fae78c67d7949c4c00
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 17:22:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   array.each do |num|
     square_array = []
     count = 0 
-    if count < num.length-1
+    if count < array.length-1
       square_array = num * num
       count +=1
     end
